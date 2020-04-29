2047a6ca13c23fb4e92579ae1ca558bb3dc5aef7
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2020-04-29 18:59:47 -0300

    #7

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Se_aprofundando_em_Python
  material de aprendizagem 
+
+Aula 7
+
+Operadores Aritméticos
+
+   5 + 2 == 7 <-- adição              5 ** 2 == 25 <-- potencialização
+   5 - 2 == 3 <-- subtração             5 // 2 == 4 <-- divisão inteira
+   5 * 2 == 10 <-- multiplicação            5 % 2 == 1 <-- resto
+   5 / 2 == 2,5 <-- divisão
+
+Ordem de precêdencia 
+
+1º ()
+2° **
+3º *, /, //, %
+4º +, -
+
+Exemplos
+
+    5 + 3 * 2 == 11
+    3 * 5 + 4 ** 2 == 31
+    3 * (5 + 4) ** 2 == 243
--- mundo_1.py
+++ mundo_1.py
@@ -1,24 +1,4 @@
-#   AULA 6   
-print ('Olá, Mundo!');
+#   AULA 7
 
-nome = input('Qual é o seu nome? ')
-idade = input ('Qual a sua idade? ')
-peso = input ('qual o sue peso? ')
-
-print (nome, idade, peso);
-
-#Exercício 1
-nome = input ('Qual é o seu nome? ')
-print ('Olá', (nome),', seja muito bem-vindo ! ')
-
-#Exercício 2
-dia = input('Dia = ')
-mes = input('Mês = ')
-ano = input ('Ano = ')
-print ('Você nasceu no dia', (dia), 'de', (mes), 'de', (ano), '. Correto?')
-
-#Exercício 03
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-soma = int (n1+n2)
-print ('O valor {} e {} soma {}'.format(n1, n2, soma))
+nome =(input('Qual é o seu nome? '))
+print ('Olá {}, prazer em te conhecer'.format(nome))
