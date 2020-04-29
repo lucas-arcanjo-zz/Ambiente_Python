46c125f85639d24fdf4e18a565330ec324fdfaf4
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2020-04-29 17:18:06 -0300

    correção

---
+++ mundo_1.py
@@ -0,0 +1,23 @@
+print ('Olá, Mundo!');
+
+nome = input('Qual é o seu nome? ')
+idade = input ('Qual a sua idade? ')
+peso = input ('qual o sue peso? ')
+
+print (nome, idade, peso);
+
+#Exercício 1
+nome = input ('Qual é o seu nome? ')
+print ('Olá', (nome),', seja muito bem-vindo ! ')
+
+#Exercício 2
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input ('Ano = ')
+print ('Você nasceu no dia', (dia), 'de', (mes), 'de', (ano), '. Correto?')
+
+#Exercício 3
+n1 = input ('Digite um número: ')
+n2 = input ('Digite outro número: ')
+s = n1+n2
+print ('A soma é', s)
