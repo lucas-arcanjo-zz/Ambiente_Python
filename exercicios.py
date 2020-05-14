fa39a4b72f9207df46035e52eb4c61b76c1c5057
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2020-05-13 23:35:22 -0300

    Incluindo desafios

--- exercicios.py
+++ exercicios.py
@@ -136,3 +136,73 @@ else:
 
     # Exercicio 35
     #  Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
+    #==============================MUNDO 2==============================#
+    
+    #Desafio 036
+#Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar
+#Calcule o valor da prestação mensalm sabendo que ela não pode exceder 30% do salário ou entçaio o empréstimo será negado.
+
+#Desafio 037
+#Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
+# - 1 para binário
+# - 2 para octal
+# - 3 hexadecimal
+
+#Desafio 038
+#Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
+# - 0 primeiro valor é maior
+# - 0 segundo valor é maior
+# - Não existe valor maior, os dois são iguais
+
+#Desafio 039
+#Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
+
+# - Se ele ainda vai se alistar ao serviço militar.
+# - Se é a hora de se alistar.
+# - Se já passou do tempo do alistamento.
+
+#Seu programa também deverá mostrar o tempo que falta ou que passou o prazo.
+
+#Desafio 040
+#Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+
+# - Média abaixo de 5.0: REPROVADO
+# - Média entre 5.0 e 6.9: RECUPERAÇÃO
+# - Média 7.0 ou superior: APROVADO
+
+#Desafio 041
+#A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
+
+# Até 9 anos: MIRIM
+# Até 14 anos: INFANTIL
+# Até 19 anos: JUNIOR
+# Até 20 anos: SÊNIOR
+# Acima: MASTER
+
+#Desafio 042
+#Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+
+# Equilátero: todos os lados iguais.
+# Isósceles: dois lados iguais.
+# Escaleno: todos os lados diferentes
+
+#Desafio 043
+#Desenvolva uma lógica de leia o peso e a altura de uma pessoa, calculce seu IMC e mostre seu status, de acordo com a tabela abaixo:
+
+# - Abaixo de 18.5: Abaixo do Peso
+# - Entre 18.5 e 25: Peso ideal
+# - 25 até 30: Sobrepeso
+# - 30 até 40: Obesidade
+# - Acima de 40: Obesidade mórbida
+
+#Desafio 044
+#Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+
+# - à vista dinheiro/cheque: 10% de desconto
+# - à vista no cartão: 5% de desconto
+# - em até 2x no cartão: preço normal
+# - 3x ou mais no cartão: 20% de juros
+
+#Desafio 045
+#Crie um programa que faça o computador jogar Jokenpô com você.
