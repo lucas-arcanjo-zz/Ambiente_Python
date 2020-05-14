4401015b93f1e85bfc1887a94c413692254a4ee2
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2020-05-14 18:16:18 -0300

    RESOLVENDO DESAFIO 047

--- exercicios.py
+++ exercicios.py
@@ -234,3 +234,10 @@ for c in range(10,-1,-1):
     sleep(1.5)
     print(c)
 print ('BUM, BUM, POOOW!')
+
+##Desafio 047
+#Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+for c in range (2, 51, 2):
+    print(c, end=' ')   
+print ('ACABOU')
